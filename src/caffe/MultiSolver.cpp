7beb0f392f34e3cb85cbee540047b28185c57271
zjian <zjian@stanford.edu>
2016-10-10 15:49:21 -0700

    test like master branch

--- src/caffe/MultiSolver.cpp
+++ src/caffe/MultiSolver.cpp
@@ -49,25 +49,25 @@ Dtype MultiSolver<Dtype>::ForwardBackwardImpl(bool first, bool last) {
     }
     root_solver_->net()->BackwardFromTo(i, i);
 
-    // // DEBUG
-    // if (i == 12) {
-    //   int mpi_size;
-    //   int param_server_rank;
-    //   int mpi_rank;
-    //   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-    //   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-    //   // Blob<Dtype>* blob = blob_accessor->get_blob(layer_id, 0);
+    // DEBUG
+    if (i == 12) {
+      int mpi_size;
+      int param_server_rank;
+      int mpi_rank;
+      MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+      MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+      // Blob<Dtype>* blob = blob_accessor->get_blob(layer_id, 0);
 
-    //   Dtype val = 0.0;
-    //   // for (int i = 0; i < this->net().layers()[12]->blobs()[0]->count(); i++) {
-    //     // val += this->net().layers()[12]->blobs()[0]->cpu_diff()[i];
-    //     val += this->net().layers()[12]->blobs()[0]->cpu_diff()[0];
-    //   // }
+      Dtype val = 0.0;
+      // for (int i = 0; i < this->net().layers()[12]->blobs()[0]->count(); i++) {
+        // val += this->net().layers()[12]->blobs()[0]->cpu_diff()[i];
+        val += this->net().layers()[12]->blobs()[0]->cpu_diff()[0];
+      // }
 
 
-    //   LOG(INFO) << "blob 12 0 generated " << val << " mpi rank " << mpi_rank;
+      LOG(INFO) << "blob 12 0 generated " << val << " mpi rank " << mpi_rank;
 
-    // }
+    }
 
 
 
