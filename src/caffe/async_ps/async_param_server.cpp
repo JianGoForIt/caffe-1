5bb28f087fbf72605be3696bd3d2a48774707173
zjian <zjian@stanford.edu>
2016-12-27 14:23:51 -0800

    refactor into profiling header

--- src/caffe/async_ps/async_param_server.cpp
+++ src/caffe/async_ps/async_param_server.cpp
@@ -7,16 +7,9 @@
 #include "caffe/async_ps/async_param_server.hpp"
 #include "caffe/internode/mpiutil.hpp"
 
-// PROFILING
+// Modified by Jian
+#include "caffe/async_ps/profiling.hpp"
 
-#define PROFILE_BEGIN(name)      \
-    LOG(INFO) << caffe::internode::mpi_get_current_proc_rank_as_string()    \
-              << " PROFILING BEGIN[" << name << "]"
-
-              
-#define PROFILE_END(name)      \
-    LOG(INFO) << caffe::internode::mpi_get_current_proc_rank_as_string()    \
-              << " PROFILING END[" << name << "]"
 
 
 namespace caffe {
--- src/caffe/multinode/SynchronousNode.cpp
+++ src/caffe/multinode/SynchronousNode.cpp
@@ -67,18 +67,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // #include <sys/types.h>
 // #include <unistd.h>
 #include <mpi.h>
-
-
-// PROFILING
-
-#define PROFILE_BEGIN(name)      \
-    LOG(INFO) << caffe::internode::mpi_get_current_proc_rank_as_string()    \
-              << " PROFILING BEGIN[" << name << "]"
-
-              
-#define PROFILE_END(name)      \
-    LOG(INFO) << caffe::internode::mpi_get_current_proc_rank_as_string()    \
-              << " PROFILING END[" << name << "]"
+#include "caffe/async_ps/profiling.hpp"
 
 
 namespace caffe {
